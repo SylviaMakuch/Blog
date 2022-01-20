6e432280ef66334f4bfc4fabc7a5cd487727c9bd
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-19 20:27:18 -0800

    server set up w/ mongoose

--- server/index.js
+++ server/index.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.url({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://sylviamakuch:q0i1S6wSK4KqtAxl@cluster0.hh4yz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
