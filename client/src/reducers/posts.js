86d631c4faed16cb0311a1d8514f669c595956f4
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-21 15:59:07 -0800

    reducers - initial set up

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,9 @@
+const reducers = (posts = [], action) =>{
+    switch(action.type) {
+        case "FETCH_ALL":
+            return posts;
+        case "CREATE":
+            return posts;
+        default:
+            return posts;
+    }}
