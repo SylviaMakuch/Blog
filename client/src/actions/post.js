be463f24136a5ffdc508041502ac325641bc5c56
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-22 19:28:57 -0800

    Update post.js

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,11 +4,12 @@ const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts;
 
-    dispatch({ type: "FETCH_ALL", payload: [] });
-  } catch (error) {}
-  dispatch(action);
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {
+  console.log(error.message);
+  }
 };
 //Action Creators, function returning actions, payload= stored data//
 // when working with async fx, we can use R.thunk, allowing us to specify an additional arrow fx which we will use async
 //now we have access to dispatch w/async//
-//catching all the data from API
+//catching all the data from API, data reps the post
