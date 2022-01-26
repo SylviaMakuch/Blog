ab1040e5cc4ac5946b4d0ac11c7834ec63b74ff4
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-25 19:53:58 -0800

    post.js is made w/ MUI components and actions relating to our page

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -1,21 +1,31 @@
 import React from "react";
 // import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from "react-redux";
+import Post from "../Post/post";
 
 import Post from "./Post/Post";
-const MediaDiv = styled.div`
+const MainDiv = styled.div`
   display: flex;
+`;
+
+const Card = styled.div`
+  height: 100px;
 `;
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   console.log(Posts);
-  return (
-   !posts.length ? <CircularProgress /> : (
-     <MediaDiv>
-
-     </MediaDiv>
-   );
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <MainDiv>
+      {posts.map((post) => (
+        <Card key={post.id}>
+          <Post post={post} />
+        </Card>
+      ))}
+    </MainDiv>
+  );
 };
 
 export default Posts;
