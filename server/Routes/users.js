ef05ca5ce90154d419834e03fb17079464f15adf
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-08 11:55:34 -0800

    Server: controller for sign in and sign out logic

--- server/Routes/users.js
+++ server/Routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signin, signout } from "../controllers/user";
+import { signin, signout } from "../controllers/user.js";
 const router = express.Router();
 
 router.post("/signin", signin);
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,27 +1,56 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import express from 'express';
-import mongoose from 'mongoose';
-import PostMessage from '../models/postMessage.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+// import express from 'express';
+// import mongoose from 'mongoose';
+import User from "../models/user.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-export const signin = async (req, res) => { 
-    try {
-        const postMessages = await PostMessage.find();
-                
-        res.status(200).json(postMessages);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+export const signin = async (req, res) => {
+  const { email, password } = req.params;
 
-export const signout = async (req, res) => { 
-    try {
-        const postMessages = await PostMessage.find();
-                
-        res.status(200).json(postMessages);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "User does not exist" });
+    const isPasswordCorrect = await bcrypt(password, existingUser.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Incorrect password/email" });
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong with that login!" });
+  }
+};
+
+export const signout = async (req, res) => {
+  const { email, password, confimedPassword, firstName, lastName } = req.params;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exist" });
+
+    if (password != confimedPassword);
+    return res.status(400).json({ message: "password does not match!" });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName}``${lastName}`,
+    });
+
+    const token = jwt.sign({ email: result.email, id: result._id }, "test", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ result: result, token });
+  } catch {
+    res.status(500).json({ message: "Something went wrong with that login!" });
+  }
+};
