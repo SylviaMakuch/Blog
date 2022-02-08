b01ee8a26b36efb3d7a6c292dffba0cfb5773d93
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-08 12:03:57 -0800

    fixed hashed out code in Controler:User,

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -28,25 +28,27 @@ export const signout = async (req, res) => {
   const { email, password, confimedPassword, firstName, lastName } = req.params;
 
   try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser)
-      return res.status(400).json({ message: "User already exist" });
+    const oldUser = await UserModal.findOne({ email });
 
-    if (password != confimedPassword);
-    return res.status(400).json({ message: "password does not match!" });
+    if (oldUser)
+      return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
-    const result = await User.create({
+
+    const result = await UserModal.create({
       email,
       password: hashedPassword,
-      name: `${firstName}``${lastName}`,
+      name: `${firstName} ${lastName}`,
     });
 
-    const token = jwt.sign({ email: result.email, id: result._id }, "test", {
+    const token = jwt.sign({ email: result.email, id: result._id }, secret, {
       expiresIn: "1h",
     });
-    res.status(200).json({ result: result, token });
-  } catch {
-    res.status(500).json({ message: "Something went wrong with that login!" });
+
+    res.status(201).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+
+    console.log(error);
   }
 };
