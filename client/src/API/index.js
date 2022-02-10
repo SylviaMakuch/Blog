a69c5718a2bbdf86292434026a177154c05235e1
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-09 21:43:58 -0800

    API axios urls added for signin/sign up

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,8 +1,12 @@
-import axios from 'axios';
-const API  = axios.create({ baseURL:'http://localhost:5001' })
+import axios from "axios";
+const API = axios.create({ baseURL: "http://localhost:5001" });
 
-export const fetchPosts = () => API.get('/posts');
-export const createPost = (newPost) => API.post('/posts', newPost);
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
+
+export const signin = (FormData) => API.post("/users/signin", FormData);
+export const signup = (FormData) => API.post("/users/signup", FormData);
